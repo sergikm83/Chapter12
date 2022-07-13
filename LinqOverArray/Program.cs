f8ac65815d349486da1b28238a0211db8827cc63
Sergey Misko <spy_3462@mail.ru>
2022-07-13 17:03:48 +0400

    добавил статический метод QueryOverStrings

--- LinqOverArray/Program.cs
+++ LinqOverArray/Program.cs
@@ -8,5 +8,10 @@ namespace LinqOverArray
         {
             Console.WriteLine("Hello World!");
         }
+        static void QueryOverStrings()
+        {
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+        }
+
     }
 }
