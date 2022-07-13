234f92e9a2db2276ed7decbd11860ca993d4fdf0
Sergey Misko <sergik.m@outlook.com>
2022-07-13 23:33:07 +0500

    создал List<Car> в Main и заполнил его данными

--- ListOverCollections/Program.cs
+++ ListOverCollections/Program.cs
@@ -10,6 +10,14 @@ namespace ListOverCollections
         {
             Console.WriteLine("***** LINQ over Generic Collections *****\n");
             // Создать список List<> объектов Car.
+            List<Car> myCars = new List<Car>()
+            {
+                new Car { PetName = "Henry", Color = "Silver", Speed = 100, Make = "BMW" },
+                new Car { PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW" },
+                new Car { PetName = "Mary", Color = "Black", Speed = 55, Make = "VW" },
+                new Car { PetName = "Clunker", Color = "Rust", Speed = 5, Make = "Yugo" },
+                new Car { PetName = "Melvin", Color = "White", Speed = 43, Make = "Ford" }
+            };
         }
     }
 }
