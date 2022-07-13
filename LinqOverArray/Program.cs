a400a29e9589a128793a33430fbe3e5852efe330
Sergey Misko <spy_3462@mail.ru>
2022-07-13 16:57:07 +0400

    Добавьте файлы проекта.

---
+++ LinqOverArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqOverArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
