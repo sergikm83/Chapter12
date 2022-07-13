a8304147689de5219cd6641b3911095d6e78c8fc
Sergey Misko <spy_3462@mail.ru>
2022-07-13 17:12:39 +0400

    в методе QueryOverStrings() добавил linq запрос и результат вывел на консоль

--- LinqOverArray/Program.cs
+++ LinqOverArray/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqOverArray
 {
@@ -11,6 +13,12 @@ namespace LinqOverArray
         static void QueryOverStrings()
         {
             string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+            IEnumerable<string> subset = from g in currentVideoGames
+                                         where g.Contains(" ")
+                                         orderby g
+                                         select g;
+            foreach (string s in subset)
+                Console.WriteLine("Item: {0}", s);
         }
 
     }
