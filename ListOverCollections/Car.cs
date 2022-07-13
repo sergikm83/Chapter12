bbb44903e18b36b82b465f94c444df96069f6942
Sergey Misko <sergik.m@outlook.com>
2022-07-13 23:21:39 +0500

    добавил класс Car

---
+++ ListOverCollections/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOverCollections
+{
+    class Саг
+    {
+        public string PetName { get; set; } = "";
+        public string Color { get; set; } = "";
+        public int Speed { get; set; }
+        public string Make { get; set; } = "";
+    }
+}
