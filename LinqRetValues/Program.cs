0b598faf216708817587a218103720b969b8ebfa
Sergey Misko <sergik.m@outlook.com>
2022-07-13 23:09:36 +0500

    добавил метод GetStringSubset()

--- LinqRetValues/Program.cs
+++ LinqRetValues/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqRetValues
 {
@@ -7,6 +9,13 @@ namespace LinqRetValues
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        static IEnumerable<string> GetStringSubset()
+        {
+            string[] colors = {"Black","Light Red", "Green", "White",
+            "Yellow", "Dark Red","Pink","Purple","Silver","Red"};
+            IEnumerable<string> theRedColors = from c in colors where c.Contains("Red") select c;
+            return theRedColors;
         }
     }
 }
