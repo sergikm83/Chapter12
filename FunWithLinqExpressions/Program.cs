d485531d4be0ac49ce89f16e39fcb102fe6bb085
Sergey Misko <spy_3462@mail.ru>
2022-07-14 15:54:27 +0400

    создал массив ProductInfo[] в Main и заполнил его данными

--- FunWithLinqExpressions/Program.cs
+++ FunWithLinqExpressions/Program.cs
@@ -6,7 +6,49 @@ namespace FunWithLinqExpressions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Query Expressions *****\n");
+            // Создадим массив для тестов...
+            ProductInfo[] itemsInStock = new[]
+            {
+                new ProductInfo
+                {
+                    Name = "Mac's Coffee",
+                    Description = "Coffee with TEETH",
+                    NumberInStock = 24
+                },
+                new ProductInfo
+                {
+                    Name = "Milk Maid Milk",
+                    Description = "Milk cow's love",
+                    NumberInStock = 100
+                },
+                new ProductInfo
+                {
+                    Name = "Pure Silk Tofu",
+                    Description = "Bland as Possible",
+                    NumberInStock = 120
+                },
+                new ProductInfo
+                {
+                    Name = "Crunchy Pops",
+                    Description = "Cheezy, peppery goodness",
+                    NumberInStock = 2
+                },
+                new ProductInfo
+                {
+                    Name = "RipOff Water",
+                    Description = "From the tap to your wallet",
+                    NumberInStock = 100
+                },
+                new ProductInfo
+                {
+                    Name = "Classic Valpo Pizza",
+                    Description = "Everyone loves pizza",
+                    NumberInStock = 73
+                }
+            };
+            // Здесь будем вызывать разнообразные методы!!!
+
         }
     }
 }
