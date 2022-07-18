820a075c7ffe1f697dcc998cef5a7d9ed3d5233e
Sergey Misko <spy_3462@mail.ru>
2022-07-18 13:24:43 +0400

    добавил класс MediaPlayer

---
+++ LazyObjectlnstantiation/Classes/MediaPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyObjectlnstantiation.Classes
+{
+    // Объект MediaPlayer имеет объекты AllTracks.
+    class MediaPlayer
+    {
+        // Предположим, что эти методы делают что-то полезное.
+        public void Play() { }
+        public void Pause() { }
+        public void Stop() { }
+        private AllTracks allSongs = new AllTracks();
+        public AllTracks GetAllTracks()
+        {
+            // вернуть все композиции.
+            return allSongs;
+        }
+    }
+}
