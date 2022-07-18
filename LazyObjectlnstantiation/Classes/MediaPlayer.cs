2ddefd283c7eb369037fed52232fa8a186e6b910
Sergey Misko <spy_3462@mail.ru>
2022-07-18 14:10:38 +0400

    Добавил вывод в консоль сообщения при создании объекта AllTracks

--- LazyObjectlnstantiation/Classes/MediaPlayer.cs
+++ LazyObjectlnstantiation/Classes/MediaPlayer.cs
@@ -13,7 +13,13 @@ namespace LazyObjectlnstantiation.Classes
         public void Play() { }
         public void Pause() { }
         public void Stop() { }
-        private Lazy<AllTracks> allSongs = new Lazy<AllTracks>();
+        // Использовать лямбда-выражение для добавления дополнительного
+        // кода, который выполняется при создании объекта AllTracks.
+        private Lazy<AllTracks> allSongs = new Lazy<AllTracks>(()=>
+        {
+            Console.WriteLine("Creating AllTracks object!");
+            return new AllTracks();
+        });
         public AllTracks GetAllTracks()
         {
             // вернуть все композиции.
